413d955d6dc620a51bfda5a1f0f7f79798d1a22d
omegleforindia <sunnyking2680@gmail.com>
2025-06-16 17:54:47 +0530

    server.js

--- server.js
+++ server.js
@@ -80,21 +80,7 @@ io.on("connection", (socket) => {
   const p = partners.get(socket.id);
   if (p) io.to(p).emit("message", msg);
 
-socket.on("join-video", () => {
-  socket.broadcast.emit("ready");
-});
 
-socket.on("offer", (offer) => {
-  socket.broadcast.emit("offer", offer);
-});
-
-socket.on("answer", (answer) => {
-  socket.broadcast.emit("answer", answer);
-});
-
-socket.on("candidate", (candidate) => {
-  socket.broadcast.emit("candidate", candidate);
-});
    
  });
   socket.on("typing", () => {
